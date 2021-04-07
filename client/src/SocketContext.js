e8dcf803520eeba976a8f4811910d0171f250609
Deepak Ranjan <rranjan306@gmail.com>
2021-04-07 22:37:20 +0530

    Update socket url

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -3,7 +3,7 @@ import { io, Socket } from "socket.io-client";
 import Peer from "simple-peer";
 
 const SocketContext = createContext();
-const socket = io("http://localhost:5000");
+const socket = io("https://react-webrct-video-chat.herokuapp.com/");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState();
